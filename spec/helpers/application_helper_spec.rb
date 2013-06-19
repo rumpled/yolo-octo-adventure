1431cbbbb2694a5ba4d5289a327fe0d77673853f
Jason Blank <jason@geekcompendium.net>
2013-06-19 17:41:00 -0400

    Lesson 5 complete custom css, and SCSS partials (_NAME.html.erb as naming convention) re-do the layout add routes for static pages controller methods users controller signup route cleaned up testing specs

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  describe 'full_title' do
+    it "should include the page title" do
+      full_title('foo').should =~ /foo/
+    end
+    it "should include the base title" do
+      full_title('foo').should =~ /^Jason/
+    end
+    it "should not include a pipe on the home page" do
+      full_title('').should_not =~ /\|/
+    end
+  end
+end
